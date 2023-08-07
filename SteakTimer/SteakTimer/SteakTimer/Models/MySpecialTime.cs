fd8e0edcc73f7f835670c916f5fc73a68872193e
troshca <111294697+troshca@users.noreply.github.com>
2023-08-07 21:22:01 +0300

    Add time pickers, pause for turning
    UWP: edit name

---
+++ SteakTimer/SteakTimer/SteakTimer/Models/MySpecialTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteakTimer.Models
+{
+    public class MySpecialTime
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public MySpecialTime(string name, int value) 
+        { 
+            Name = name;
+            Value = value;
+        }
+
+        public override string ToString()
+        { 
+            return Name; 
+        }
+    }
+}
--- SteakTimer/SteakTimer/SteakTimer/ViewModels/SteakTimerViewModel.cs
+++ SteakTimer/SteakTimer/SteakTimer/ViewModels/SteakTimerViewModel.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using Xamarin.Forms;
 using SkiaSharp;
+using SteakTimer.Models;
 
 namespace SteakTimer.ViewModels
 {
@@ -32,6 +33,18 @@ namespace SteakTimer.ViewModels
         }
         private Stages _currentStage { get; set; }
         private bool _started {  get; set; }
+        private int _pauseTime;
+        public int PauseTime
+        {
+            get { return _pauseTime; }
+            set { SetProperty(ref _pauseTime, value); }
+        }
+        private bool _addPause;
+        public bool AddPause
+        {
+            get { return _addPause; }
+            set { SetProperty(ref _addPause, value); }
+        }
 
         private int _degreesOfCircle {  get; set; }
         public ObservableValue FirstCrust { get; set; }
@@ -48,22 +61,68 @@ namespace SteakTimer.ViewModels
         public double SecondFriedDiff { get; set; }
         public IEnumerable<ISeries> Series { get; set; }
 
+        //Pickers
+        private List<MySpecialTime> _timesForPickers { get; set; }
+        private List<MySpecialTime> _crustTimePickerCollection;
+        public List<MySpecialTime> CrustTimePickerCollection
+        {
+            get { return _crustTimePickerCollection; }
+            set { SetProperty(ref _crustTimePickerCollection, value); }
+        }
+
+        private MySpecialTime _pickerCrustSelection;
+        public MySpecialTime PickerCrustSelection
+        {
+            get { return _pickerCrustSelection; }
+            set { SetProperty(ref _pickerCrustSelection, value); }
+        }
+        private List<MySpecialTime> _friedTimePickerCollection;
+        public List<MySpecialTime> FriedTimePickerCollection
+        {
+            get { return _friedTimePickerCollection; }
+            set { SetProperty(ref _friedTimePickerCollection, value); }
+        }
+        private MySpecialTime _pickerFriedSelection;
+        public MySpecialTime PickerFriedSelection
+        {
+            get { return _pickerFriedSelection; }
+            set { SetProperty(ref _pickerFriedSelection, value, () => RaisePropertyChanged(nameof(FirstCrust))); }
+        }
+
         public SteakTimerViewModel(INavigationService navigationService) : base(navigationService)
         {
+            //Picker
+            _timesForPickers = new List<MySpecialTime>();
+            for(int i = 10; i <= 10 * 6 * 10; )
+            {
+                if( i / 60 >= 1 )
+                {
+                    if((i - (i / 60) * 60) == 0)
+                    {
+                        _timesForPickers.Add(new MySpecialTime((i / 60).ToString() + " мин. ", i));
+                    }
+                    else
+                    {
+                        _timesForPickers.Add(new MySpecialTime((i / 60).ToString() + " мин. " + ((i - (i / 60) * 60)).ToString() + " сек.", i));
+                    }
+                }
+                else
+                {
+                    _timesForPickers.Add(new MySpecialTime(i.ToString() + " сек.", i));
+                }
+                i += 10;
+            }       
+
+            //Fill pickers
+            CrustTimePickerCollection = _timesForPickers;
+            FriedTimePickerCollection = _timesForPickers;
+
+            //Select default
+            PickerCrustSelection = _timesForPickers[5];
+            PickerFriedSelection = _timesForPickers[8];
+
             //Degrees of Circle
             _degreesOfCircle = 360;
-
-            //Times
-            FirstCrustTimeInSeconds = 60;
-            SecondCrustTimeInSeconds = 60;
-            FirstFriedTimeInSeconds = 90;
-            SecondFriedTimeInSeconds = 90;
-
-            //Degrees per second
-            FirstCrustDiff = _degreesOfCircle / FirstCrustTimeInSeconds;
-            SecondCrustDiff = _degreesOfCircle / SecondCrustTimeInSeconds;
-            FirstFriedDiff = _degreesOfCircle / FirstFriedTimeInSeconds;
-            SecondFriedDiff = _degreesOfCircle / SecondFriedTimeInSeconds;
 
             //Degrees
             FirstCrust = new ObservableValue { Value = _degreesOfCircle };
@@ -71,7 +130,11 @@ namespace SteakTimer.ViewModels
             FirstFried = new ObservableValue { Value = _degreesOfCircle };
             SecondFried = new ObservableValue { Value = _degreesOfCircle };
 
-            //
+            //Pause
+            AddPause = true;
+            PauseTime = 5;
+
+            //Chart
             Series = new GaugeBuilder()
                 .WithOffsetRadius(5)
                 .WithLabelsSize(0)
@@ -81,6 +144,7 @@ namespace SteakTimer.ViewModels
                 .AddValue(SecondFried, "SecondFriedTime", SKColors.SaddleBrown)
                 .BuildSeries();
 
+            //Buttons
             StartCommand = new DelegateCommand(Start);
         }
 
@@ -88,6 +152,24 @@ namespace SteakTimer.ViewModels
         {
             if (_started) return;
             _started = true;
+
+            FirstCrust.Value = _degreesOfCircle;
+            SecondCrust.Value = _degreesOfCircle;
+            FirstFried.Value = _degreesOfCircle;
+            SecondFried.Value = _degreesOfCircle;
+
+            //Times
+            FirstCrustTimeInSeconds = PickerCrustSelection.Value;
+            SecondCrustTimeInSeconds = PickerCrustSelection.Value;
+            FirstFriedTimeInSeconds = PickerFriedSelection.Value;
+            SecondFriedTimeInSeconds = PickerFriedSelection.Value;
+
+            //Degrees per second
+            FirstCrustDiff = _degreesOfCircle / FirstCrustTimeInSeconds / 2;
+            SecondCrustDiff = _degreesOfCircle / SecondCrustTimeInSeconds / 2;
+            FirstFriedDiff = _degreesOfCircle / FirstFriedTimeInSeconds / 2;
+            SecondFriedDiff = _degreesOfCircle / SecondFriedTimeInSeconds / 2;
+
             for (_currentStage = Stages.FirstCrust; _currentStage <= Stages.SecondFried; _currentStage++)
             {
                 switch (_currentStage)
@@ -97,13 +179,15 @@ namespace SteakTimer.ViewModels
                             for (;;)
                             {
                                 FirstCrust.Value -= FirstCrustDiff;
-                                await Task.Delay(1000);
+                                await Task.Delay(500);
                                 if (FirstCrust.Value <= 0) 
 								{
 									FirstCrust.Value = 0;
 									break;
 								}
                             }
+                            if(AddPause)
+                                await Task.Delay(PauseTime * 1000);
                             break;
                         }
                     case Stages.SecondCrust:
@@ -111,13 +195,15 @@ namespace SteakTimer.ViewModels
                             for (; ; )
                             {
                                 SecondCrust.Value -= SecondCrustDiff;
-                                await Task.Delay(1000);
+                                await Task.Delay(500);
                                 if (SecondCrust.Value <= 0)
 								{
 									SecondCrust.Value = 0;
 									break;
 								}
                             }
+                            if (AddPause)
+                                await Task.Delay(PauseTime * 1000);
                             break;
                         }
                     case Stages.FirstFried:
@@ -125,13 +211,15 @@ namespace SteakTimer.ViewModels
                             for (; ; )
                             {
                                 FirstFried.Value -= FirstFriedDiff;
-                                await Task.Delay(1000);
+                                await Task.Delay(500);
                                 if (FirstFried.Value <= 0)
 								{
 									FirstFried.Value = 0;
 									break;
 								}
                             }
+                            if (AddPause)
+                                await Task.Delay(PauseTime * 1000);
                             break;
                         }
                     case Stages.SecondFried:
@@ -139,13 +227,15 @@ namespace SteakTimer.ViewModels
                             for (; ; )
                             {
                                 SecondFried.Value -= SecondCrustDiff;
-                                await Task.Delay(1000);
+                                await Task.Delay(500);
                                 if (SecondFried.Value <= 0)
                                 {
                                     SecondFried.Value = 0;
                                     break;
                                 }
                             }
+                            if (AddPause)
+                                await Task.Delay(PauseTime * 1000);
                             break;
                         }
                 }
